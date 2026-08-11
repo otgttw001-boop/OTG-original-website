6355b33504c9c6f1717ba4103f6d159b6e950c3e
OTG <otgttw.001@gmail.com>
2026-08-11 16:04:07 +0100

    Fix missing App import in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from 'src/App.jsx'; // <--- REMOVE THE CURLY BRACES!
+import App from './App.jsx';
+// or simply (if you omit the extension, Vite will reso // <--- REMOVE THE CURLY BRACES!
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
