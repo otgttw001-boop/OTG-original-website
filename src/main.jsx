48945b6c7e68b1aaa6922923f1d6a457da15f53e
OTG <otgttw.001@gmail.com>
2026-08-11 15:02:37 +0100

    Fix App import and export structure

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import App from './App.jsx' // <--- MAKE SURE THIS IMPORT LINE IS HERE!
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
 )
