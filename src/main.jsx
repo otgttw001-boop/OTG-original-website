8eed4154f7691eb60e07af0ab7750250cf15a3ed
OTG <otgttw.001@gmail.com>
2026-08-11 15:21:40 +0100

    Fix missing App import in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.jsx'; // <--- MAKE SURE THIS IS IMPORTED
+import { App } from './App.jsx' // <--- MAKE SURE THIS IS IMPORTED
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
